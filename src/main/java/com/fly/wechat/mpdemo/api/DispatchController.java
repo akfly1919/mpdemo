bc0db9a7a22a0cf58b61d047164066b0fdcf1568
wangfei3 [王飞] <wangfei3@xiaomi.com>
2018-02-09 17:53:16 +0800

    dd

--- src/main/java/com/fly/wechat/mpdemo/api/DispatchController.java
+++ src/main/java/com/fly/wechat/mpdemo/api/DispatchController.java
@@ -67,7 +67,7 @@ public class DispatchController extends BaseController {
 	private String baseString(Map<String, String> map){
 		Map<String, String> baseMap=new HashMap<String,String>();
 		baseMap.put("CreateTime", System.currentTimeMillis()+"");
-		baseMap.put("MsgType", "news");
+		baseMap.put("MsgType", "text");
 		baseMap.put("Content", "正在调试");
 		baseMap.put("ToUserName", map.get("FromUserName"));
 		baseMap.put("FromUserName", map.get("ToUserName"));
